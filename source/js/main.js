b65a639fad88f447b8769fe44df590bd99a275b7
m-wellerson <m_wellerson@outlook.com>
2020-12-15 15:33:34 -0300

    Update

--- source/js/main.js
+++ source/js/main.js
@@ -7,11 +7,11 @@ window.onscroll = function() {
 };
 
 let darkmode = {
-    '--primary-color' : { dark: '#222222', light: '#e3e0d7'},
-    '--border-color'  : { dark: '#fff', light: '#161616' },
-    '--grey'          : { dark:  '#fff', light: '#3b3b3b' },
-    '--glass'         : { dark:  'hsla(0, 0%, 0%, 0.5)', light: 'hsla(0, 0%, 100%, 0.5)' },
-    '--border-color'  : { dark:  '#fff', light: '#161616' },
+    '--primary-color'    : { dark: '#222222', light: '#e3e0d7'},
+    '--border-color'     : { dark: '#fff', light: '#161616' },
+    '--grey'             : { dark:  '#fff', light: '#3b3b3b' },
+    '--glass'            : { dark:  'hsla(0, 0%, 0%, 0.5)', light: 'hsla(0, 0%, 100%, 0.5)' },
+    '--bg-font-variable' : { dark:  '#0B1335', light: '#fb3729' },
 };
 
 const sunMoonContainer = document.querySelector('.sun-moon-container')
@@ -21,12 +21,17 @@ document.querySelector('.lamp_mode-color').addEventListener('click', () => {
     const currentRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation')) + 180;
     if(darkModeSet) {
         sunMoonContainer.style.setProperty('--rotation', currentRotation);
-        
+        document.querySelector('.dark-mode-img').removeAttribute('hidden');
+        document.querySelector('.ligh-mode-img').setAttribute('hidden', '');
+
         Object.entries(darkmode).forEach(items => {
             document.documentElement.style.setProperty(items[0], items[1].dark);
         });
     }else{
         sunMoonContainer.style.setProperty('--rotation', currentRotation);
+        document.querySelector('.dark-mode-img').setAttribute('hidden', '');
+        document.querySelector('.ligh-mode-img').removeAttribute('hidden');
+
         Object.entries(darkmode).forEach(items => {
             document.documentElement.style.setProperty(items[0], items[1].light);
         });
